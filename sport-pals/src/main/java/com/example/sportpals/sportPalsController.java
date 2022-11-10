6c62c8b58a0625abd5d84e0d358109f00cbb6e1e
iamsolas <t8170182@aueb.gr>
2022-11-10 20:07:09 +0200

    Create Sign Up Pane

--- sport-pals/src/main/java/com/example/sportpals/sportPalsController.java
+++ sport-pals/src/main/java/com/example/sportpals/sportPalsController.java
@@ -1,17 +1,66 @@
 package com.example.sportpals;
 
 import javafx.fxml.FXML;
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
-import java.util.Date;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+import javafx.scene.control.ComboBox;
 
-public class sportPalsController {
+import java.net.URL;
+import java.util.ResourceBundle;
 
-    Date currentDate = new Date();
+public class sportPalsController implements Initializable {
+
+    private boolean logInVisible = true;
+
     @FXML
-    private Label welcomeText;
+    private Label btnSignUp;
 
     @FXML
-    protected void onHelloButtonClick() {welcomeText.setText(String.valueOf(currentDate));}
+    private Pane pnSignIn;
 
+    @FXML
+    private Pane pnSignUp;
 
+    @FXML
+    private Label btnBackSignUp;
+
+    @FXML
+    private ComboBox<String> cityComboBox;
+
+    @FXML
+    private ComboBox<String> sportsComboBox;
+
+    @FXML
+    void changeWindow(ActionEvent event) {
+
+    }
+
+    @FXML
+    void createAccount(MouseEvent event) {
+        pnSignIn.setVisible(!logInVisible);
+        pnSignIn.setDisable(logInVisible);
+        pnSignUp.setVisible(logInVisible);
+        pnSignUp.setDisable(!logInVisible);
+    }
+
+    @FXML
+    void signInPane(MouseEvent event) {
+        pnSignIn.setVisible(logInVisible);
+        pnSignIn.setDisable(!logInVisible);
+        pnSignUp.setVisible(!logInVisible);
+        pnSignUp.setDisable(logInVisible);
+    }
+    //Set strings to ComboBox
+    private String[] city = {"Athens", "Thesalloniki", "Patra"};
+    private String[] sports = {"Football", "Basketball", "Tennis"};
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        cityComboBox.getItems().addAll(city);
+        sportsComboBox.getItems().addAll(sports);
+    }
+    //end ComboBox
 }
